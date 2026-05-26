559cce5c86416648104b8d5d33edd2830d6ef9a2
lhoangasync <vatcvietmy123456@gmail.com>
2026-05-26 23:24:40 +0700

    feat: wire up homepage with all sections

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div></div>;
+import { HeroSection }     from "./_components/HeroSection";
+import { ProductsSection } from "./_components/ProductsSection";
+import { FeaturesSection } from "./_components/FeaturesSection";
+import { StatsSection }    from "./_components/StatsSection";
+import { MarqueeSection }  from "./_components/MarqueeSection";
+import { CtaSection }      from "./_components/CtaSection";
+
+export default function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <ProductsSection />
+      <FeaturesSection />
+      <StatsSection />
+      <MarqueeSection />
+      <CtaSection />
+    </>
+  );
 }
