70514bed71e7ef04e08b5554d423565c35b10a64
lhoangasync <vatcvietmy123456@gmail.com>
2026-05-27 11:11:31 +0700

    fix: add role=img and size fallback to ZaloIcon

--- client/lib/icons.tsx
+++ client/lib/icons.tsx
@@ -3,8 +3,11 @@ export function ZaloIcon({ className }: { className?: string }) {
     <svg
       className={className}
       viewBox="0 0 24 24"
+      width="1em"
+      height="1em"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
+      role="img"
       aria-label="Zalo"
     >
       <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.4 16.5H7.1a.6.6 0 0 1-.6-.6v-.35l7.65-8.05H7.4a.6.6 0 0 1-.6-.6V6.6c0-.33.27-.6.6-.6h9.9c.33 0 .6.27.6.6v.5l-7.55 7.9H17.4c.33 0 .6.27.6.6v.8c0 .33-.27.6-.6.6z" />
